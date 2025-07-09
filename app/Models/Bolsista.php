81a5fd834f495e683ad4ee9ebd1a0f965faed360
Andre Reis <andrealvesreiscontato@gmail.com>
2025-07-09 16:16:50 -0300

    refactor: adds missing fields to the Bolsista model

--- app/Models/Bolsista.php
+++ app/Models/Bolsista.php
@@ -7,11 +7,51 @@ use Illuminate\Database\Eloquent\Model;
 class Bolsista extends Model
 {
     protected $fillable = [
+        // Vínculo
         'user_id',
+        'status_solicitacao',
+
+        // Dados pessoais
         'nome',
+        'data_nasc',
+        'nome_mae',
+        'nome_pai',
+        'estado_civil',
+        'raca_cor',
+        'telefone',
         'email',
-        'status_solicitacao'
+        'escolaridade',
+
+        // Naturalidade
+        'muni_nasc',
+        'uf_nasc',
+
+        // Endereço
+        'cep',
+        'muni_resid',
+        'uf_resid',
+        'logradouro',
+        'numero',
+        'complemento',
+        'bairro',
+
+        // Documentos
+        'cpf',
+        'pis',
+        'rg',
+        'rg_orgao',
+        'rg_orgao_uf',
+        'rg_data_emissao',
+        'titulo_eleitor',
+        'titulo_zona',
+        'titulo_secao',
+        'certificado_reservista',
+
+        // Banco
+        'agencia',
+        'conta',
     ];
+
 
     public function user()
     {
