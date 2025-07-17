381f453b3f40501570adbba627c0b37abb58f28b
Andre Reis <andrealvesreiscontato@gmail.com>
2025-07-17 13:57:39 -0300

    feat: adds Bolsa factory

---
+++ database/factories/BolsaFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bolsa>
+ */
+class BolsaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => Str::uuid(),
+            'user_id' => 1,
+            'token' => Str::uuid(),
+            'nome' => $this->faker->name(),
+        ];
+    }
+}
