28975801d6e4df855cc7d56c66e03916723c9a0c
Andre Reis <andrealvesreiscontato@gmail.com>
2025-07-17 18:56:43 -0300

    feat: adds a Mailer class that deals with the 'SolicitarBolsa' method

---
+++ app/Mail/SolicitarBolsa.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Bolsa;
+use Illuminate\Bus\Queueable;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Mail\Attachment;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class SolicitarBolsa extends Mailable
+{
+    public Bolsa $bolsa;
+    public array $files;
+
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Bolsa $bolsa, array $files)
+    {
+        $this->bolsa = $bolsa;
+        $this->files = $files;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Solicitação de Bolsa',
+            from: Auth::user()->email,
+            replyTo: Auth::user()->email
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.bolsa',
+            with: [
+                'bolsa' => $this->bolsa->nome,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        $attachments = [];
+
+        $pdf = PDF::loadView('pdfs.doc_heteroidentificacao');
+
+        $attachments[] = Attachment::fromData(fn() => $pdf->output(), 'doc_heteroidentificacao.pdf')->withMime('application/pdf');
+
+        foreach ($this->files as $file) {
+            if($file instanceof UploadedFile){
+                $attachments[] = Attachment::fromPath($file->getRealPath())
+                    ->as($file->getClientOriginalName())
+                    ->withMime($file->getClientMimeType());
+            }
+        }
+
+        return $attachments;
+    }
+}
