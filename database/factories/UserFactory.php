9c3aa3d0e4c0524eb927bc4f368a431e9274867e
Andre Reis <andrealvesreiscontato@gmail.com>
2025-07-24 17:38:27 -0300

    refactor: suits the project to the Spatie permission standards

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,6 @@ class UserFactory extends Factory
             'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'role' => 'test',
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,14 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $admin = Role::create(['name' => env('ADMIN_ROLE')]);
+        $projetos = Role::create(['name' => 'projetos']);
+        $rh = Role::create(['name' => 'rh']);
 
-        User::factory()->create([
+        Permission::create(['name' => 'administrar bolsa']);
+        Permission::create(['name' => 'administrar usuario']);
+
+        $user = User::factory()->create([
             'name' => env('ADMIN_NAME'),
             'username' => env('ADMIN_USERNAME'),
             'password' => env('ADMIN_PASSWORD'),
-            'role' => env('ADMIN_ROLE'),
             'email' => env('ADMIN_EMAIL'),
         ]);
+
+        $admin->givePermissionTo([
+            'administrar bolsa',
+            'administrar usuario',
+        ]);
+
+        $projetos->givePermissionTo([
+            'administrar bolsa',
+        ]);
+
+        $user->assignRole(env('ADMIN_ROLE'));
     }
 }
