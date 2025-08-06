70e39ec160de06babbe0e9c9e1e97010e97fdfe7
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-05 23:50:11 -0300

    feat: adds a middleware that verifies if a Bolsa token is valid

---
+++ app/Http/Middleware/ValidToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Services\BolsaServiceInterface;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidToken
+{
+    public function __construct(protected BolsaServiceInterface $bolsaService)
+    {
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if($request->session('token') == null || $this->bolsaService->checkToken($this->bolsaService->findBolsaByToken($request->session('token')->value)) == false){
+            return redirect()->route('bolsistas.login');
+        }
+
+        return $next($request);
+    }
+}
