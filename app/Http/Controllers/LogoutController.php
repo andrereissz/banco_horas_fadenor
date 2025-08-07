78d74fb5892c5380da011f1a1c92d6c7391de1bf
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-07 17:20:35 -0300

    refactor: moves the LogoutController to its correct directory

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Services\Interfaces\AuthServiceInterface;
+use Illuminate\Http\Request;
+
+class LogoutController
+{
+    public function __construct(protected AuthServiceInterface $authService){}
+
+    public function destroy(Request $request)
+    {
+        $this->authService->logout($request);
+        return redirect('/');
+    }
+}
