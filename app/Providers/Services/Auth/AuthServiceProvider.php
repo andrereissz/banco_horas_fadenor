f056a45148a9b512b0887b17d55a9c24fc94cbe5
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-07 17:19:35 -0300

    fix: makes the AuthServiceProvider track the current reffered route

--- app/Providers/Services/Auth/AuthServiceProvider.php
+++ app/Providers/Services/Auth/AuthServiceProvider.php
@@ -5,20 +5,25 @@ namespace App\Providers\Services\Auth;
 use App\Services\Bolsistas\AuthBolsistaService;
 use App\Services\Fundacao\AuthUserService;
 use App\Services\Interfaces\AuthServiceInterface;
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AuthServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->bind(AuthServiceInterface::class, function ($app) {
-            $request = $app->make('request');
-            if($request->is('fundacao/*')) {
-                return new AuthUserService();
-            }
-            else if($request->is('bolsistas/*')) {
+            $request = $app->make(Request::class);
+            $refererUrl = $request->headers->get('referer');
+            if($refererUrl && Str::contains($refererUrl, '/bolsistas')) {
                 return new AuthBolsistaService();
             }
+            if($refererUrl && Str::contains($refererUrl, '/fundacao')) {
+                return new AuthUserService();
+            }
+
+            return new AuthUserService();
         });
     }
 
