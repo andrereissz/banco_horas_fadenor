5731d96566f3f1d69369970565b65478d00d6986
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-08 18:51:48 -0300

    fix: fixes the issue that the SendSolicitacaoMail job wasn't sending mails

--- app/Jobs/SendSolicitacaoMail.php
+++ app/Jobs/SendSolicitacaoMail.php
@@ -6,9 +6,7 @@ use App\Enums\BolsaStatus;
 use App\Mail\SolicitarBolsa;
 use App\Models\Bolsa;
 use App\Models\User;
-use App\Providers\BolsaServiceProvider;
-use App\Services\BolsaService;
-use App\Services\BolsaServiceInterface;
+use App\Services\Interfaces\Bolsas\BolsaServiceInterface;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Mail;
@@ -17,30 +15,34 @@ class SendSolicitacaoMail implements ShouldQueue
 {
     use Queueable;
 
+    protected BolsaServiceInterface $bolsaService;
+
     /**
      * Create a new job instance.
      */
     public function __construct(protected User $authenticatedUser, protected Bolsa $bolsa, protected string $bolsistaNome,protected string $emailDestinatario)
-    {}
+    {
+        $this->bolsaService = app(BolsaServiceInterface::class);
+    }
 
     /**
      * Execute the job.
      */
-    public function handle(BolsaServiceInterface $bolsaService): void
+    public function handle(): void
     {
-        $bolsaService->updateStatus($this->bolsa, BolsaStatus::AguardandoEnvio);
+        $this->bolsaService->updateStatus($this->bolsa, BolsaStatus::AguardandoEnvio);
         try {
             Mail::to($this->emailDestinatario)
                 ->send(new SolicitarBolsa($this->authenticatedUser, $this->bolsa, $this->bolsistaNome));
 
-            $bolsaService->updateStatus($this->bolsa, BolsaStatus::AguardandoResposta);
+            $this->bolsaService->updateStatus($this->bolsa, BolsaStatus::AguardandoResposta);
         } catch (\Exception $e) {
             throw $e;
         }
     }
 
-    public function failed(BolsaServiceInterface $bolsaService, \Throwable $exception): void
+    public function failed(\Throwable $exception): void
     {
-        $bolsaService->updateStatus($this->bolsa, BolsaStatus::Erro);
+        $this->bolsaService->updateStatus($this->bolsa, BolsaStatus::Erro);
     }
 }
