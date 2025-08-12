f33fb046d4d5c23adbffd3f73441d6ec1d03eed6
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-12 16:28:32 -0300

    refactor: fixes code standards

--- resources/views/livewire/bolsistas/auth/login-form.blade.php
+++ resources/views/livewire/bolsistas/auth/login-form.blade.php
@@ -1,46 +1,49 @@
-<form wire:submit.prevent="authenticate">
-    @csrf
-    <div href="/" class="flex flex-col gap-2 justify-center items-center">
+<div>
+    <div>
         <a href="/" class="flex flex-col gap-2 justify-center items-center">
             <img src="{{ asset('storage/images/logo-oficial-fadenor.png') }}" alt="Logo Fadenor" class="h-24">
         </a>
         <h3 class="2xl font-bold">ACESSO BOLSISTAS</h3>
         <div class="divider divider-vertical"></div>
     </div>
-    <!-- cpf -->
-    <div>
-        <x-input-label for="cpf" :value="'CPF'" />
-        <x-text-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')" required
-            autofocus autocomplete="cpf" wire:model.defer="cpf" />
-        <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
-    </div>
+    <form wire:submit.prevent="authenticate">
+        @csrf
 
-    <!-- Password -->
-    <div class="mt-4">
-        <x-input-label for="password" :value="'Senha'" />
+        <!-- cpf -->
+        <div>
+            <x-input-label for="cpf" :value="'CPF'" />
+            <x-text-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')"
+                required autofocus autocomplete="cpf" wire:model.defer="cpf" />
+            <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
+        </div>
 
-        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-            autocomplete="current-password" wire:model.defer="password" />
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="'Senha'" />
 
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-    </div>
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" wire:model.defer="password" />
 
-    <!-- Remember Me -->
-    <div class="block mt-4">
-        <label for="remember_me" class="inline-flex items-center">
-            <input id="remember_me" type="checkbox"
-                class="checkbox rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember"
-                wire:model="remember">
-            <span class="ms-2 text-sm text-gray-600">Manter-me conectado</span>
-        </label>
-    </div>
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
 
-    <div class="flex items-center justify-end mt-4 gap-4">
-        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            href="{{ route('bolsistas.password.request') }}">
-            Esqueceu a senha?
-        </a>
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox"
+                    class="checkbox rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                    name="remember" wire:model="remember">
+                <span class="ms-2 text-sm text-gray-600">Manter-me conectado</span>
+            </label>
+        </div>
 
-        <button class="btn btn-primary w-1/4" type="submit">Login</button>
-    </div>
-</form>
+        <div class="flex items-center justify-end mt-4 gap-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('bolsistas.password.request') }}">
+                Esqueceu a senha?
+            </a>
+
+            <button class="btn btn-primary w-1/4" type="submit">Login</button>
+        </div>
+    </form>
+</div>
--- resources/views/livewire/fundacao/auth/login-form.blade.php
+++ resources/views/livewire/fundacao/auth/login-form.blade.php
@@ -1,5 +1,4 @@
-<form wire:submit.prevent="authenticate">
-    @csrf
+<div>
     <div href="/" class="flex flex-col gap-2 justify-center items-center">
         <a href="/" class="flex flex-col gap-2 justify-center items-center">
             <img src="{{ asset('storage/images/logo-oficial-fadenor.png') }}" alt="Logo Fadenor" class="h-24">
@@ -7,40 +6,43 @@
         <h3 class="2xl font-bold">ACESSO COLABORADORES</h3>
         <div class="divider divider-vertical"></div>
     </div>
-    <!-- Username -->
-    <div>
-        <x-input-label for="username" :value="'Usuário'" />
-        <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required
-            autofocus autocomplete="username" wire:model.defer="username" />
-        <x-input-error :messages="$errors->get('username')" class="mt-2" />
-    </div>
+    <form wire:submit.prevent="authenticate">
+        @csrf
+        <!-- Username -->
+        <div>
+            <x-input-label for="username" :value="'Usuário'" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
+                required autofocus autocomplete="username" wire:model.defer="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
 
-    <!-- Password -->
-    <div class="mt-4">
-        <x-input-label for="password" :value="'Senha'" />
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="'Senha'" />
 
-        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-            autocomplete="current-password" wire:model.defer="password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" wire:model.defer="password" />
 
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-    </div>
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
 
-    <!-- Remember Me -->
-    <div class="block mt-4">
-        <label for="remember_me" class="inline-flex items-center">
-            <input id="remember_me" type="checkbox"
-                class="checkbox rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember"
-                wire:model="remember">
-            <span class="ms-2 text-sm text-gray-600">Manter-me conectado</span>
-        </label>
-    </div>
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox"
+                    class="checkbox rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                    name="remember" wire:model="remember">
+                <span class="ms-2 text-sm text-gray-600">Manter-me conectado</span>
+            </label>
+        </div>
 
-    <div class="flex items-center justify-end mt-4 gap-4">
-        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            href="{{ route('fundacao.password.request') }}">
-            Esqueceu a senha?
-        </a>
+        <div class="flex items-center justify-end mt-4 gap-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('fundacao.password.request') }}">
+                Esqueceu a senha?
+            </a>
 
-        <button class="btn btn-primary w-1/4" type="submit">Login</button>
-    </div>
-</form>
+            <button class="btn btn-primary w-1/4" type="submit">Login</button>
+        </div>
+    </form>
+</div>
