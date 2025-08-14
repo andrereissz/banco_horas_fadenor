bfb23822a33cc43b16c4e7f12a8315b5f36aa4bf
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-14 14:10:57 -0300

    fix: fixes the ValidToken Middleware

--- app/Http/Middleware/ValidToken.php
+++ app/Http/Middleware/ValidToken.php
@@ -7,6 +7,8 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use function PHPUnit\Framework\isNull;
+
 class ValidToken
 {
     public function __construct(protected BolsaServiceInterface $bolsaService) {}
@@ -18,9 +20,19 @@ class ValidToken
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->session('token') == null || $this->bolsaService->checkToken($this->bolsaService->findBolsaByToken($request->session('token')->value)) == false) {
+        $token = $request->route('bolsa_token');
+
+        if ($token == null || $this->bolsaService->findBolsaByToken($request->route('bolsa_token')) == null) {
+            flash()->error('Solicitação de bolsa inexistente.');
             return redirect()->route('bolsistas.login');
         }
+
+        if(!$this->bolsaService->checkToken($this->bolsaService->findBolsaByToken($request->route('bolsa_token')))) {
+            flash()->error('Solicitação de bolsa expirada.');
+            return redirect()->route('bolsistas.login');
+        }
+
+        $request->attributes->set('bolsa_token', $token);
 
         return $next($request);
     }
