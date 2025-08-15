034672caabaa09ffedfcdba7e94fcc5856e79157
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-15 16:18:44 -0300

    feat: adds a input with Telefone mask

---
+++ resources/views/components/inputs/input-telefone.blade.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\View\Components\Inputs;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class InputTelefone extends Component
+{
+    public string $name;
+    public ?string $id;
+    public ?string $value;
+    public bool $required;
+    public string $label;
+    public string $class;
+    public string $placeholder;
+
+    public function __construct(
+        string $name = 'telefone',
+        ?string $id = null,
+        ?string $value = null,
+        bool $required = false,
+        string $label = 'Telefone',
+        string $class = 'input input-bordered w-full',
+        string $placeholder = '(00) 00000-0000',
+    ) {
+        $this->name = $name;
+        $this->id = $id;
+        $this->value = $value;
+        $this->required = $required;
+        $this->label = $label;
+        $this->class = $class;
+        $this->placeholder = $placeholder;
+    }
+
+    public function render(): View|string
+    {
+        return view('components.inputs.input-telefone');
+    }
+}
