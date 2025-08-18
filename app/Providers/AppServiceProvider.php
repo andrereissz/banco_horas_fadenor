f9ba4396960d4e9ef47e4ad3560057fbe5b594e9
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-18 17:18:37 -0300

    feat: adds a service that consumes the ViaCep API

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Interfaces\ViaCep\ViaCepServiceInterface;
+use App\Services\ViaCep\ViaCepService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,7 +11,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void {}
+    public function register(): void {
+        $this->app->bind(ViaCepServiceInterface::class, function ($app) {
+            return new ViaCepService;
+        });
+    }
 
     /**
      * Bootstrap any application services.
