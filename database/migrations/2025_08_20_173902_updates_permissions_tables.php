22d236c260174eefebfc8b2bc4357416353cf1ec
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-20 14:44:30 -0300

    feat: adds a migration that updates the Spatie model_has_* tables, adds uuids

---
+++ database/migrations/2025_08_20_173902_updates_permissions_tables.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table('model_has_permissions', function (Blueprint $table) {
+            $table->uuid('model_id')->change();
+        });
+
+        Schema::table('model_has_roles', function (Blueprint $table) {
+            $table->uuid('model_id')->change();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table('model_has_permissions', function (Blueprint $table) {
+            $table->unsignedBigInteger('model_id')->change();
+        });
+
+        Schema::table('model_has_roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('model_id')->change();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+};
