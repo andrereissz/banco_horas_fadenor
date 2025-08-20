4878c0d334a254452e35eae5a82ccb00ccb1f14e
Andre Reis <andrealvesreiscontato@gmail.com>
2025-08-20 15:47:41 -0300

    refactor: checks if the token is valid to be accessed

--- app/Http/Middleware/ValidToken.php
+++ app/Http/Middleware/ValidToken.php
@@ -22,18 +22,12 @@ class ValidToken
     {
         $token = $request->route('bolsa_token');
 
-        if ($token == null || $this->bolsaService->findBolsaByToken($request->route('bolsa_token')) == null) {
-            flash()->error('Solicitação de bolsa inexistente.');
-            return redirect()->route('bolsistas.login');
+        if ($this->bolsaService->checkToken($this->bolsaService->findBolsaByToken($token))) {
+            $request->attributes->set('bolsa_token', $token);
+            return $next($request);
         }
 
-        if(!$this->bolsaService->checkToken($this->bolsaService->findBolsaByToken($request->route('bolsa_token')))) {
-            flash()->error('Solicitação de bolsa expirada.');
-            return redirect()->route('bolsistas.login');
-        }
-
-        $request->attributes->set('bolsa_token', $token);
-
-        return $next($request);
+        flash()->info('Solicitação de bolsa expirada ou inexistente.');
+        return redirect()->route('bolsistas.login');
     }
 }
